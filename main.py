b46d01d551f78c71e15bb082fbe83d01e1e16ce5
psyberprimate <oskuronnberg@hotmail.com>
2023-12-07 20:29:40 +0200

    update #11 - main changes

--- main.py
+++ main.py
@@ -35,10 +35,13 @@ def main(config_name, weight_index): # MORL
     #co = coadapt.Coadaptation(config) # ORIG 
     co = coadapt.Coadaptation(config_name, weight_index, project_name, run_name) # MORL
     # Set custom seed
-    seed = 1
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
+    if seed:
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        print(f"Custom seed set: {seed}")
+    else:
+        print(f"Custom seed set not set, using random seed")
     #run coadapt
     co.run()
     wandb.finish()
@@ -46,19 +49,32 @@ def main(config_name, weight_index): # MORL
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 4:
+    if len(sys.argv) > 5:
         config_name = cfg.config_dict[sys.argv[1]]
         weight_index = int(sys.argv[2])
         project_name = sys.argv[3]
         run_name = sys.argv[4]
+        seed = int(sys.argv[5])
         print(run_name)
         print(project_name)
         #Later on needs to changed to give you a option to give trackable names for wandb tracking
         print(f"index w : {weight_index}")
         print(config_name['weights'][weight_index])
+    elif len(sys.argv) > 4:
+        config_name = cfg.config_dict[sys.argv[1]]
+        weight_index = int(sys.argv[2])
+        project_name = sys.argv[3]
+        run_name = sys.argv[4]
+        seed = False
+        print(run_name)
+        print(project_name)
+        #Later on needs to changed to give you a option to give trackable names for wandb tracking
+        print(f"index w : {weight_index}")
+        print(config_name['weights'][weight_index]) 
     else:
         config_name = cfg.config_dict['sac_pso_sim']
         weight_index = 0
+        seed = False
         project_name="coadapt-save-testing"
         run_name="default-run-weight" + f"-{config_name['weights'][weight_index]}"
         print(config_name['weights'][weight_index])
