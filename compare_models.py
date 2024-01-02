713080b1186cbef6546fb6684669f94872b6a4e7
psyberprimate <oskuronnberg@hotmail.com>
2024-01-02 18:33:25 +0200

    #update 1 - fix

--- compare_models.py
+++ compare_models.py
@@ -1,13 +1,13 @@
 import csv
+from colorsys import hls_to_rgb
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import plotly.graph_objects as go
 
 ### CANNOT BE USED WITH OLD CSV FILES ###
 ### NEW VERSION ###
 
-<<<<<<< Updated upstream
-=======
 path='/home/oskar/Thesis/inter/model_comparison_results' # paths need to be correct
 newline=''
 
@@ -25,177 +25,13 @@ def get_distinct_colors(n):
     return colors
 
 
->>>>>>> Stashed changes
 def convert_key_to_tuple(key):
     #return a tuple of values based on which the keys are sorted
     key_values = key.split('_')
     key_values = [value for value in key_values if value] 
-    #print(tuple(map(float, key_values)))
     return tuple(map(float, key_values))
 
-path='/home/oskar/Thesis/model_comparison_results' # paths need to be correct
-path_link='/home/oskar/Thesis/Model_scalarized/results_with_rescaling/random seed/' # remember to set the directories correctly
-newline=''
 
-<<<<<<< Updated upstream
-value_sums = {}
-value_sums_mean = {}
-link_lengths = {}
-
-#if __name__ == "__main__": 
-
-for directoryname in os.listdir(path):
-    directorypath = os.path.join(path, directoryname)
-    #Set up dictionaries
-    value_sums_mean[directoryname] = {}
-    value_sums[directoryname] = {}
-    link_lengths[directoryname] = {}
-    if os.path.isdir(directorypath):
-        for directoryname2 in os.listdir(directorypath):
-            #Set new directory path
-            directorypath2 = os.path.join(directorypath, directoryname2)
-            
-            directory_keyname = directoryname2.split('[')[-1:]
-            directory_keyname = directory_keyname[0].replace("]", "_").replace(", ", "_")
-            #print(f"directory path 2: {directorypath2}")
-            # Go through and save data to dictionaries
-            if os.path.isdir(directorypath2):
-                total_run_spd_reward = np.array([]) #reset when in new file
-                total_energy_cons_reward = np.array([])
-                link_lenghts_ind = np.array([])
-                
-                for filename in os.listdir(directorypath2):
-                    if filename.endswith(".csv"):
-                        #print(filename)
-                        filepath = os.path.join(directorypath2, filename)
-                        with open(filepath, newline=newline) as file:
-                            reader = csv.reader(file)
-                            rows = [] # list for read values
-                            running_speed_reward = np.array([])#reset when in new file
-                            energy_consumption_reward = np.array([])
-                            
-                            total_run_spd_reward = np.array([]) #reset when in new directory
-                            total_energy_cons_reward = np.array([])
-                            link_lenghts_ind = np.array([])
-                            for row in reader:
-                                rows.append(row)
-                                
-                            run_speed_sum_reward_sum = np.sum(np.array(rows[1], dtype=float))
-                            energy_cons_reward_sum = np.sum(np.array(rows[2], dtype=float))
-                            link_lenghts_ind = np.append(link_lenghts_ind, np.array(rows[0], dtype=float))
-                            total_run_spd_reward = np.append(total_run_spd_reward, run_speed_sum_reward_sum)
-                            total_energy_cons_reward = np.append(total_energy_cons_reward, energy_cons_reward_sum)
-                            value_sums_mean[directoryname][directory_keyname] = {'running_speed_returns_sum_mean':np.mean(total_run_spd_reward), 'energy_consumption_returns_sum_mean':np.mean(total_energy_cons_reward)}
-                            link_lengths[directoryname][directory_keyname] = link_lenghts_ind
-                            value_sums[directoryname][directory_keyname] = {'running_speed_returns_sum': total_run_spd_reward , 'energy_consumption_returns_sum': total_energy_cons_reward}
-
-# Sort dictionaries based on keys
-sorted_mean_value_sums = dict(sorted(value_sums_mean.items(), key=lambda item: convert_key_to_tuple(item[0])))
-sorted_value_sums = dict(sorted(value_sums.items(), key=lambda item: convert_key_to_tuple(item[0])))
-sorted_link_lengths = dict(sorted(link_lengths.items(), key=lambda item: convert_key_to_tuple(item[0]))) #Sort link lenghts
-labels_links = list(sorted_link_lengths.keys())
-
-# Sort the inner dictionaries based on keys
-# mean values
-for key, inner_dict in sorted_mean_value_sums.items():
-    #print(inner_dict.items())
-    sorted_mean_value_sums[key] = dict(sorted(inner_dict.items(), key=lambda item: convert_key_to_tuple(item[0])))
-# sums
-for key, inner_dict in sorted_value_sums.items():
-    sorted_value_sums[key] = dict(sorted(inner_dict.items(), key=lambda item: convert_key_to_tuple(item[0]))) 
-# link lengths
-for key, inner_dict in sorted_link_lengths.items():
-    sorted_link_lengths[key] = dict(sorted(inner_dict.items(), key=lambda item: convert_key_to_tuple(item[0]))) 
-
-#Calculate values
-reward_sums = np.array([(sorted_mean_value_sums[key1][key2]['running_speed_returns_sum_mean'], 
-                               sorted_mean_value_sums[key1][key2]['energy_consumption_returns_sum_mean']) for key1 in sorted_mean_value_sums.keys() for key2 in sorted_mean_value_sums[key1].keys()])
-
-value_std = np.array([[np.std(sorted_value_sums[key1][key2]['running_speed_returns_sum'], axis=0),
-                      np.std(sorted_value_sums[key1][key2]['energy_consumption_returns_sum'], axis=0)]
-                     for key1 in sorted_mean_value_sums.keys() for key2 in sorted_mean_value_sums[key1].keys()])
-
-#bar plot
-fig, ax = plt.subplots()
-bar_width = 0.3
-off_set = 0.15
-group_offset = 0.5
-index = np.arange(len(reward_sums))
-
-bar1 = ax.bar(index - off_set, reward_sums[:, 0], bar_width, label='Running Speed')
-bar2 = ax.bar(index + off_set, reward_sums[:, 1], bar_width, label='Energy Consumption')
-
-ax.errorbar(index - off_set, [sorted_mean_value_sums[key1][key2]['running_speed_returns_sum_mean']
-                              for key1 in sorted_mean_value_sums.keys() for key2 in sorted_mean_value_sums[key1].keys()],
-            yerr=[value_std[i][0] for i in range(len(value_std))], fmt='none', color='black', capsize=5)
-
-ax.errorbar(index + off_set, [sorted_mean_value_sums[key1][key2]['energy_consumption_returns_sum_mean']
-                              for key1 in sorted_mean_value_sums.keys() for key2 in sorted_mean_value_sums[key1].keys()],
-            yerr=[value_std[i][1] for i in range(len(value_std))], fmt='none', color='black', capsize=5)
-
-ax.set_xlabel('Weights')
-ax.set_ylabel('Mean sums')
-ax.set_title('Mean sums of Running Speed and Energy Consumption for Each Weight')
-ax.set_xticks(index)
-ax.set_xticklabels([key2 for key1 in sorted_mean_value_sums.keys() for key2 in sorted_mean_value_sums[key1].keys()], rotation=45, ha='right')
-ax.legend()
-
-
-#scatter plot
-fig2, ax2 = plt.subplots()
-ax2.set_ylabel('Energy')
-ax2.set_xlabel('Speed')
-ax2.set_title('Mean sums of Running Speed and Energy Consumption for Each Weight')
-
-unique_weight_groups = sorted(set([key1 for key1 in sorted_mean_value_sums.keys()]))
-color_dict = {weight_group: plt.get_cmap('rainbow')(i / len(unique_weight_groups)) for i, weight_group in enumerate(unique_weight_groups)}
-#"color_dict = {weight_group: plt.get_cmap('tab10')(i) for i, weight_group in enumerate(unique_weight_groups)} # WORK WITH ONLY 10 colors # use viridian if 
-legend_added = {} # keep track of added legends for weight groups
-
-for index, (key1, key2) in enumerate([(key1, key2) for key1 in sorted_mean_value_sums.keys() for key2 in sorted_mean_value_sums[key1].keys()]):
-    mask = [key_compare == key1 for key_compare in sorted_mean_value_sums.keys()]
-    weight_group = unique_weight_groups[np.where(mask)[0][0]]  # Find the index where the mask is True
-    if weight_group not in legend_added:
-        ax2.scatter(reward_sums[index, 0], reward_sums[index, 1], s=50, color=color_dict[weight_group], label=weight_group)   
-        legend_added[weight_group] = True
-    else:
-        ax2.scatter(reward_sums[index, 0], reward_sums[index, 1], s=50, color=color_dict[weight_group])
-        
-# Annotate points on the scatter plot
-for index, txt in enumerate([key2 for key1 in sorted_mean_value_sums.keys() for key2 in sorted_mean_value_sums[key1].keys()]):
-    ax2.annotate(txt, (reward_sums[index, 0], reward_sums[index, 1]), textcoords="offset points", xytext=(0, 10), ha='center')
-
-ax2.errorbar(reward_sums[:, 0], reward_sums[:, 1],
-    xerr=[value_std[i][0] for i in range(len(value_std))],
-    yerr=[value_std[i][1] for i in range(len(value_std))],
-    fmt='_', capsize=5, color='black', label='Error bars')
-
-ax2.legend()
-
-#DOES NOT WORK PROPERLY
-
-# test_amount = 5
-# num_figures = len(labels_links) // test_amount
-
-# link_lengths_array_mean = np.array([])
-# for i in range(num_figures) : link_lengths_array_mean = np.append(link_lengths_array_mean,np.mean(link_lengths_array[0][0+test_amount*i:test_amount+test_amount*i], axis=0))
-
-# # Plotting link lengths and adding average link lengths as a red line
-# for fig_num in range(num_figures):
-#     plt.figure(figsize=(15, 12))  # Adjust the figure size as needed
-#     for i in range(test_amount):
-#         index = fig_num * test_amount + i
-#         if index < len(labels_links):
-#             plt.subplot(test_amount, 1, i + 1)
-#             #plt.bar(range(len(link_lengths_array[index])), link_lengths_array[i], label=labels_links[index])
-#             plt.axhline(y=link_lengths_array_mean[index], color='red', linestyle='--', label='Average')
-#             plt.xlabel('Link Index')
-#             plt.ylabel('Link Lengths')
-#             plt.title(f'Link Lengths for {labels_links[index]}', color='orange')
-#             plt.legend()
-
-plt.show()
-=======
 def sort_dictionaries(path):
     #Set up dictionaries
     value_sums = {}
@@ -407,4 +243,3 @@ if __name__ == "__main__":
         fig.show()
     
     plt.show()
->>>>>>> Stashed changes
--- test_policy.py
+++ test_policy.py
@@ -11,20 +11,16 @@ import numpy as np
 # put path to folder of model here, seed, weight and model folder name, aka last three parts from path
 # example -> set_seed/0.0_1.0/Thu_Dec__7_20:55:59_2023__0f1677df[0.0, 1.0]
 
-<<<<<<< Updated upstream
-path_to_folder = '/home/oskar/Thesis/Model_scalarized/results_with_rescaling/set_seed/0.8_0.2/Thu_Dec__7_20:56:48_2023__878baf28[0.8, 0.2]' 
-
-=======
 path_to_folder = '/home/oskar/Thesis/inter/models/results_with_rescaling/set_seed/0.7_0.3/Fri_Dec_29_22:47:42_2023__1186cf06[0.7, 0.3]_1' 
 
 seed = path_to_folder[-1]
->>>>>>> Stashed changes
 newline=''
 
 experiment_config = experiment_configs.sac_pso_sim #MORL dictiornary need for batch or sim
 weight_index = 5 # dummy value for creating the class, we dont update the network so the weight doesnt matter here
 project_name = "test" #"coadapt-scaled-test"#"coadapt-testing-scaling-tests"
 run_name = "test" #"0, 1, 5"#"1, 0, 7"
+
 
 def find_checkpoint(path_to_directory):
     """ Find the checkpoint for the model
@@ -40,6 +36,7 @@ def find_checkpoint(path_to_directory):
         return max(checkpoints)
     else:
         return None
+    
     
 def read_morphology(path, checkpoint) -> list:
     
@@ -57,10 +54,12 @@ def read_morphology(path, checkpoint) -> list:
                     rows.append(row)
     return rows
 
+
 if __name__ == "__main__": 
 
     #Use to get last model checkpoint
     last_model_checkpoint_num = find_checkpoint(path_to_folder)#-1 # checkpoint
+    print(last_model_checkpoint_num)
     last_model_checkpoint = f'checkpoint_design_{last_model_checkpoint_num}.chk'
     print("path_to_folder:", path_to_folder)
     print("last_model_checkpoint_num:", last_model_checkpoint_num)
@@ -77,13 +76,9 @@ if __name__ == "__main__":
     
     folder = experiment_config['data_folder'] #MORL
     rand_id = hashlib.md5(os.urandom(128)).hexdigest()[:8]
-<<<<<<< Updated upstream
-    file_str = './' + folder + '/' + time.ctime().replace(' ', '_') + '__' + rand_id + '_test_' + str(last_model_checkpoint_num)
-=======
     model_name = path_to_folder.split('/')[-1:]
     model_name = str(model_name[0])
     file_str = './' + folder + '/' + '__' + rand_id + '_' + model_name + '_test_' + str(last_model_checkpoint_num) + "_" + str(seed)
->>>>>>> Stashed changes
     experiment_config['data_folder_experiment'] = file_str # MORL
 
     #Create directory when not using video recording, turn off when you do, sloppy I know
@@ -96,9 +91,7 @@ if __name__ == "__main__":
     coadapt_test.load_networks(last_model_checkpoint)
     file_path = coadapt_test._config['data_folder_experiment'] #filepath
     n = 30 #iterations
-    #print(coadapt_test._env.get_current_design())
     coadapt_test._env.set_new_design(link_lengths) # Set new link lenghts
-    #print(coadapt_test._env.get_current_design())
     with open(
             os.path.join(file_path,
                 'episodic_rewards_run_{}.csv'.format(run_name)
@@ -110,11 +103,6 @@ if __name__ == "__main__":
             cwriter = csv.writer(fd)
             coadapt_test.initialize_episode()
             coadapt_test.execute_policy()
-            
-            # Quick note, it would be more efficient to have the results within the link lenghts themselves within results and write everything on 3 rows
-            # but that would make the previous test run files obsolete since you couldnt read the easily in the same way. This should be the way things are done later on.
-            #cwriter.writerow([coadapt_test._data_reward_1[0], coadapt_test._data_reward_2[0]]) # original
-            
             #append iteration results to lists
             running_speed.append(coadapt_test._data_reward_1[0])
             energy_saving.append(coadapt_test._data_reward_2[0])
