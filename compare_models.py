a174e40068a2b9cc4308f3b6b021e678631ada98
psyberprimate <oskuronnberg@hotmail.com>
2024-02-21 02:20:33 +0200

    fixes

--- compare_models.py
+++ compare_models.py
@@ -11,9 +11,7 @@ import plotly.graph_objects as go
 path='/home/oskar/Thesis/priori/model_comparison_results_batch'
 newline=''
 
-#save_file_name = 'link_lenght_comparison_sim' # for saving the file if needed 
-#OR switch between
-save_file_name = 'link_lenght_comparison_batch'
+save_file_name = 'link_length_comparison_' + path.split('_')[-1] # for saving the file if needed 
 
 def get_distinct_colors(n):
 
@@ -59,17 +57,17 @@ def sort_dictionaries(path):
                 directory_keyname = directory_keyname[0].replace("]", "_").replace(", ", "_")
                 # Go through and save data to dictionaries
                 if os.path.isdir(directorypath2):
-                    total_run_spd_reward = np.array([]) #reset when in new file
-                    total_energy_cons_reward = np.array([])
-                    link_lenghts_ind = np.array([])
+                    total_run_spd_reward = np.array([]) #reset when in new directory
+                    total_energy_cons_reward = np.array([]) 
                     for filename in os.listdir(directorypath2):
                         if filename.endswith(".csv"):
                             filepath = os.path.join(directorypath2, filename)
                             with open(filepath, newline=newline) as file:
                                 reader = csv.reader(file)
                                 rows = [] # list for read values
-                                total_run_spd_reward = np.array([]) #reset when in new directory
-                                total_energy_cons_reward = np.array([])
+                                # total_run_spd_reward = np.array([]) #reset when in new directory
+                                # total_energy_cons_reward = np.array([])
+                                # link_lenghts_ind = np.array([])
                                 link_lenghts_ind = np.array([])
                                 for row in reader:
                                     rows.append(row)
@@ -77,9 +75,9 @@ def sort_dictionaries(path):
                                 total_run_spd_reward = np.append(total_run_spd_reward, np.array(rows[1], dtype=float))
                                 total_energy_cons_reward = np.append(total_energy_cons_reward, np.array(rows[2], dtype=float))
                                 #values to dict
-                                value_mean[directoryname][directory_keyname] = {'running_speed_returns_mean':np.mean(total_run_spd_reward), 'energy_consumption_returns_mean':np.mean(total_energy_cons_reward)}
-                                link_lengths[directoryname][directory_keyname] = link_lenghts_ind
-                                value_std[directoryname][directory_keyname] = {'running_speed_returns_std': np.std(total_run_spd_reward) , 'energy_consumption_returns_std': np.std(total_energy_cons_reward)}
+                    value_mean[directoryname][directory_keyname] = {'running_speed_returns_mean':np.mean(total_run_spd_reward), 'energy_consumption_returns_mean':np.mean(total_energy_cons_reward)}
+                    link_lengths[directoryname][directory_keyname] = link_lenghts_ind
+                    value_std[directoryname][directory_keyname] = {'running_speed_returns_std': np.std(total_run_spd_reward) , 'energy_consumption_returns_std': np.std(total_energy_cons_reward)}
     return value_mean, value_std, link_lengths
 
 
@@ -127,31 +125,30 @@ def scatter_plot():
     legend_added = {} # keep track of added legends for weight groups
 
     #shapes of markers
-    marker_shapes = [".", ",", "o", "v", "^", "<", ">", "p", "*", "+", "h", "D", "8", ""]
+    marker_shapes = [".", ",", "o", "v", "^", "<", ">", "p", "*", "h", "D", "8", "s", ""]
+    alp_value = 0.8
 
     for index, (key1, key2) in enumerate([(key1, key2) for key1 in sorted_mean.keys() for key2 in sorted_mean[key1].keys()]):
         mask = [key_compare == key1 for key_compare in sorted_mean.keys()]
         weight_group = unique_weight_groups[np.where(mask)[0][0]]  # Find the index where the mask is True
-        
+        color = adjusted_color_dict[weight_group]
         if weight_group not in legend_added:
             shape = marker_shapes[len(legend_added) % len(marker_shapes)]
-            sc = ax2.scatter(reward_mean[index, 0], reward_mean[index, 1], s=150, color=adjusted_color_dict[weight_group], marker=shape, label=weight_group)   
+            sc = ax2.scatter(reward_mean[index, 0], reward_mean[index, 1], s=150, color=adjusted_color_dict[weight_group], marker=shape, alpha=alp_value, label=weight_group)   
             legend_added[weight_group] = True
         else:
-            sc = ax2.scatter(reward_mean[index, 0], reward_mean[index, 1], s=150, color=adjusted_color_dict[weight_group], marker=shape)
+            sc = ax2.scatter(reward_mean[index, 0], reward_mean[index, 1], s=150, color=adjusted_color_dict[weight_group], marker=shape, alpha=alp_value)
 
+        ax2.errorbar(reward_mean[index, 0], reward_mean[index, 1],
+                     xerr=ci_running_speed[index],
+                     yerr=ci_energy_consumption[index],
+                     fmt='_',
+                     capsize=0,
+                     color=color)
     ###Add or dont add annotations per model###    
     ###annote the point to scatter plot###
     # for index, txt in enumerate([key2 for key1 in sorted_mean.keys() for key2 in sorted_mean[key1].keys()]):
     #    ax2.annotate(txt, (reward_mean[index, 0], reward_mean[index, 1]), textcoords="offset points", xytext=(0, 10), ha='center')
-    
-    ax2.errorbar(reward_mean[:, 0], reward_mean[:, 1],
-        xerr=ci_running_speed,
-        yerr=ci_energy_consumption,
-        fmt='_', 
-        capsize=0, 
-        color='black', 
-        label='Error bars')
     ax2.legend()
     
     
@@ -182,7 +179,7 @@ def link_length_plot(save_file : bool = False, save_dir = 'link_length_compariso
                     marker=dict(color=color),
                     error_y=dict(
                     type='data',
-                    array=[link_lengths_std_array[i, j]],
+                    array=[ci_link_length[i, j]],
                     visible=True,
                     color=color)
                 ))
@@ -222,13 +219,18 @@ def link_length_plot(save_file : bool = False, save_dir = 'link_length_compariso
             pdf_file_path_mean = os.path.join(save_dir, f'Link_length_{j + 1}_mean_values_{save_file_name.split("_")[-1]}_.pdf')
             figm.write_image(pdf_file_path_mean, format='pdf')
             print(f'Figure saved as {pdf_file_path_mean}')
+            #save the values as pdf
+            pdf_file_path_values = os.path.join(save_dir, f'Link_length_{j + 1}_values_{save_file_name.split("_")[-1]}_.pdf')
+            figv.write_image(pdf_file_path_values, format='pdf')
+            print(f'Figure saved as {pdf_file_path_values}')
             
-        figm.show() # uncomment to see the plots 
-        figv.show() # uncomment to see the plots 
+        #figm.show() # uncomment to see the plots 
+        #figv.show() # uncomment to see the plots 
 
 
 if __name__ == "__main__":
     
+    sample_count = 5 # ADD how many samples you have...
     value_mean, value_std, link_lengths = sort_dictionaries(path)
     # Sort dictionaries based on keys
     sorted_mean = dict(sorted(value_mean.items(), key=lambda item: convert_key_to_tuple(item[0])))
@@ -260,12 +262,12 @@ if __name__ == "__main__":
     
     #Set condidence inverval
     confidency_interval = 1.96 #3.89 #1.96#2.5576 #1.96  # Give confidence interval
-    ci_running_speed = confidency_interval * (np.array([reward_std[i][0] for i in range(len(reward_std))]) / np.sqrt(len(reward_std)))
-    ci_energy_consumption = confidency_interval * (np.array([reward_std[i][1] for i in range(len(reward_std))]) / np.sqrt(len(reward_std)))
+    ci_running_speed = confidency_interval * (np.array([reward_std[i][0] for i in range(len(reward_std))]) / np.sqrt(sample_count))  # sample size is 5 since we have 5 different test runs  #np.sqrt(len(reward_std)))
+    ci_energy_consumption = confidency_interval * (np.array([reward_std[i][1] for i in range(len(reward_std))]) / np.sqrt(sample_count))
     
-    ci_link_length = confidency_interval * (link_lengths_std_array)
-    
-    
+    #ci_link_length = confidency_interval * (np.array(link_lengths_std_array / np.sqrt()))
+    sem_link_length = link_lengths_std_array / np.sqrt(5)
+    ci_link_length = confidency_interval * sem_link_length
     
     bar_plot()
     scatter_plot()
