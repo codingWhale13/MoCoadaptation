53849a90e987203acb06a3fa80bc6f6e75cd97e1
codingWhale13 <nils.kiele@gmail.com>
2024-06-14 12:37:56 +0300

    clean up main.py and delete unneeded file

--- main.py
+++ main.py
@@ -8,34 +8,39 @@ import torch
 import numpy as np
 import random
 
+
 def main(config_name, weight_index):
-    # Create foldr in which to save results
-    folder = config_name['data_folder']
-    #generate random hash string - unique identifier if we startexi
-    # multiple experiments at the same time
-    rand_id = hashlib.md5(os.urandom(128)).hexdigest()[:8]
-    if seed:
-        file_str = './' + folder + '/' + time.ctime().replace(' ', '_') + '__' + rand_id + str(config_name['weights'][weight_index]) + '_' + str(seed)# add seed to filename
+    # Random seeding
+    if seed is None:
+        print(f"Custom seed set not set, using random seed")
     else:
-        file_str = './' + folder + '/' + time.ctime().replace(' ', '_') + '__' + rand_id + str(config_name['weights'][weight_index]) # do not add the seed to filename
-    config_name['data_folder_experiment'] = file_str
-    # Create experiment folder
-    if not os.path.exists(file_str):
-      os.makedirs(file_str)
-    # Store config
-    with open(os.path.join(file_str, 'config.json'), 'w') as fd:
-            fd.write(json.dumps(config_name,indent=2))
-    co = coadapt.Coadaptation(config_name, weight_index, project_name, run_name)
-    # Set custom seed
-    if seed:
         random.seed(seed)
         np.random.seed(seed)
         torch.manual_seed(seed)
         print(f"Custom seed set: {seed}")
-    else:
-        print(f"Custom seed set not set, using random seed")
+
+    # Create foldr in which to save results
+    folder = config_name["data_folder"]
+    # generate random hash string - unique identifier if we start
+    # multiple experiments at the same time
+    rand_id = hashlib.md5(os.urandom(128)).hexdigest()[:8]
+
+    file_str = f"./{folder}/{time.ctime().replace(' ', '_')}__{rand_id}{str(config_name['weights'][weight_index])}"
+    if seed is not None:
+        file_str += f"_{seed}"
+
+    config_name["data_folder_experiment"] = file_str
+    # Create experiment folder
+    if not os.path.exists(file_str):
+        os.makedirs(file_str)
+    # Store config
+    with open(os.path.join(file_str, "config.json"), "w") as fd:
+        fd.write(json.dumps(config_name, indent=2))
+
+    # Start training
+    co = coadapt.Coadaptation(config_name, weight_index, project_name, run_name)
     co.run()
-    #wandb.finish()  Needed for wandb tracking
+    # wandb.finish()
 
 
 if __name__ == "__main__":
@@ -48,7 +53,7 @@ if __name__ == "__main__":
         print(run_name)
         print(project_name)
         print(f"index w : {weight_index}")
-        print(config_name['weights'][weight_index])
+        print(config_name["weights"][weight_index])
     elif len(sys.argv) > 4:
         config_name = cfg.config_dict[sys.argv[1]]
         weight_index = int(sys.argv[2])
@@ -58,13 +63,13 @@ if __name__ == "__main__":
         print(run_name)
         print(project_name)
         print(f"index w : {weight_index}")
-        print(config_name['weights'][weight_index]) 
+        print(config_name["weights"][weight_index])
     else:
-        #config_name = cfg.config_dict['sac_pso_sim']
-        config_name = cfg.config_dict['sac_pso_sim']
+        # config_name = cfg.config_dict['sac_pso_sim']
+        config_name = cfg.config_dict["sac_pso_sim"]
         weight_index = 0
         seed = False
-        project_name="coadapt-save-testing"
-        run_name="default-run-weight" + f"-{config_name['weights'][weight_index]}"
-        print(config_name['weights'][weight_index])
+        project_name = "coadapt-save-testing"
+        run_name = f"default-run-weight-{config_name['weights'][weight_index]}"
+        print(config_name["weights"][weight_index])
     main(config_name, weight_index)
