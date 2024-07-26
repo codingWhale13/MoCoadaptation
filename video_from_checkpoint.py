344a2c147ba1a80283c459f0e9cce094aadb4fb4
Kiele Nils <kielen1@login4.triton.aalto.fi>
2024-07-26 15:36:12 +0300

    update video utility

--- video_from_checkpoint.py
+++ video_from_checkpoint.py
@@ -1,57 +1,49 @@
 # LOADS CHECKPOINT AND SHOWS POLICY AS A VIDEO
-
 import argparse
-import json
-import os
 
 import coadapt
-from utils import add_argparse_arguments
+from utils import add_argparse_arguments, get_config, get_cycle_count
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser = add_argparse_arguments(
+    add_argparse_arguments(
         parser,
         [
-            ("data-folder", True),
-            ("save-dir", False),
+            ("run-dir", True),
+            ("design-cycle", "last"),
+            ("common-name", "episode_video"),
+            ("save-dir", "videos"),
         ],
     )
 
     return parser.parse_args()
 
 
-def generate_video(data_folder, save_dir=None):
-    # load config
-    with open(os.path.join(data_folder, "config.json")) as file:
-        config = json.load(file)
+def generate_video(run_dir, design_cycle, common_name, save_dir):
+    config = get_config(run_dir)
+    if design_cycle == "last":
+        design_cycle = get_cycle_count(run_dir)
+    else:
+        design_cycle = int(design_cycle)
 
-    config["initial_model_dir"] = data_folder  # enable model loading
-    config["use_gpu"] = False  # no need for GPU when creating videos
-    config["use_wandb"] = False  # no need for wandb when creating videos
+    config["initial_model_dir"] = run_dir  # Enable model loading
+    config["use_gpu"] = False  # No need for GPU when creating videos
+    config["use_wandb"] = False  # No need for wandb when creating videos
+    config["env"]["record_video"] = False  #  Done in create_video_of_episode
+    config["load_replay_buffer"] = False
 
-    config["env"]["record_video"] = True  # enable video recording
-    config["video"]["record_evy_n_episodes"] = 1  # we'll just run a single episode
-    if save_dir is not None:
-        config["video"]["video_save_dir"] = save_dir
-
-    # temporary stuff for backward compatibility
-    config["use_vector_q"] = config["use_vector_Q"]
-    config["scalarize_before_q_loss"] = False
-    config["condition_on_preference"] = False
-
-    co = coadapt.Coadaptation(config)  # checkpoint is loaded in __init__
-
-    # run an episode using the loaded model
-    co.initialize_episode()
-    co.execute_policy()
-
-    # actually save the video to disk
-    co._env.reset()
+    # Load checkpoint (in Coadaptation.__init__) and create video of episode
+    co = coadapt.Coadaptation(config, design_iter_to_load=design_cycle)
+    co.create_video_of_episode(save_dir, f"{common_name}_{design_cycle}")
 
 
 if __name__ == "__main__":
     args = parse_args()
-
-    generate_video(args.data_folder, args.save_dir)
+    generate_video(
+        run_dir=args.run_dir,
+        design_cycle=args.design_cycle,
+        common_name=args.common_name,
+        save_dir=args.save_dir,
+    )
